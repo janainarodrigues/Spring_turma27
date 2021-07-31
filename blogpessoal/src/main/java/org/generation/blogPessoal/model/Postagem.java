501808a5e9e25a6d28cfe23ea040167406a68b5e
janainarodrigues <janaina.rodrigues03@icloud.com>
2021-07-30 22:35:17 -0300

    Lista blog parte 3

--- blogpessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogpessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -31,11 +33,15 @@ public class Postagem {
 	private String texto;
 	
 	@NotNull
-	@Size (min = 10, max= 500)
 	@Temporal (TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
 	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private Tema tema;
+	
 	// Inserir getters e setters
+	
 	
 	public long getId() {
 		return id;
@@ -58,9 +64,15 @@ public class Postagem {
 	public Date getDate() {
 		return data;
 	}
-	public void setDate(Date date) {
-		this.data = date;
+	public void setData(Date data) {
+		this.data = data;
 	} 
 	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 }
